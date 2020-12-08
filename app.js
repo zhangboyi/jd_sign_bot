a3984dc937cd4f38ef26b72e08f423bb475b49f5
Boyi Zhang <boyi.zhang@shopee.com>
2020-12-08 15:00:58 +0800

    更新app.js

--- app.js
+++ app.js
@@ -67,6 +67,7 @@ async function start() {
     let res = t ? t[1].replace(/\n/,'') : '失败'
     let t2 = content.match(/【签到总计】:((.|\n)*)【账号总计】/)
     let res2 = t2 ? t2[1].replace(/\n/,'') : '总计0'
+    console.log("" + ` ${res2} ` + ` ${res} ` + new Date().toLocaleDateString()+",content:"+content);
 
 
     await sendNotify("" + ` ${res2} ` + ` ${res} ` + new Date().toLocaleDateString(), content);
